b9020abc22b65911d2c7760c0945319db737d1f9
Chiara Basaglia <82465313+chiarab3@users.noreply.github.com>
2023-10-02 20:11:46 +0200

    Create folders_synchronization.py

---
+++ folders_synchronization.py
@@ -0,0 +1,72 @@
+import os
+import shutil
+import hashlib
+import argparse
+import time
+from datetime import datetime
+
+def log(message, log_file=None):
+  """Logs a message to the console and to the log file"""
+  print(f"{datetime.now()}: {message}")
+  if log_file is not None:
+    with open(log_file, "a") as log_f:
+      log_f.write(f"{datetime.now()}: {message}\n")
+
+
+def sync_folders(source_folder, replica_folder, log_file):
+  """Synchronizes two folders, one way, with logging"""
+  #Get the list of all files and directories in the source folder
+  source_files = os.listdir(source_folder)
+
+  #Create a dictionary to store the MD5 checksums of all files in the source folder
+  source_file_checksums = {}
+  for file in source_files:
+    source_file_path = os.path.join(source_folder, file)
+    with open(source_file_path, "rb") as f:
+      source_file_checksums[file] = hashlib.md5(f.read()).hexdigest()
+      #print(f"CHECKSUM {file}: {source_file_checksums[file]}")
+
+  #Iterate over all files in the source folder
+  for file in source_files:
+    source_file_path = os.path.join(source_folder, file)
+    replica_file_path = os.path.join(replica_folder, file)
+
+    #If the file isn't present in the replica folder, copy it from the source folder
+    if not os.path.exists(replica_file_path):
+      log(f"Copying file {file} from source folder to replica folder", log_file)
+      shutil.copy2(source_file_path, replica_file_path)
+
+    #If the file exists in the replica folder, check if it is up to date
+    elif os.path.getmtime(source_file_path) > os.path.getmtime(replica_file_path):
+      #If the file in the replica folder is not up to date, copy it from the source folder
+      log(f"Updating file {file} in replica folder", log_file)
+      shutil.copy2(source_file_path, replica_file_path)
+
+    #If the file in the replica folder is up to date, do nothing
+    else:
+      log(f"File {file} in replica folder is up to date", log_file)
+
+  # Iterate over all files in the replica folder that are not in the source folder
+  for file in os.listdir(replica_folder):
+    if file not in source_files:
+      #If the file is not in the source folder, remove it from the replica folder
+      log(f"Removing file {file} from replica folder", log_file)
+      os.remove(os.path.join(replica_folder, file))
+
+def main(source_folder, replica_folder, sync_interval, log_file):
+  """Synchronizes periodically two folders, one way, with logging"""
+  while True:
+    sync_folders(source_folder, replica_folder, log_file)
+    time.sleep(sync_interval)
+
+if __name__ == "__main__":
+
+  parser = argparse.ArgumentParser(description="Periodic one-way synchronization of two folders, with log file")
+  parser.add_argument("source_folder", help="The path to the source folder")
+  parser.add_argument("replica_folder", help="The path to the replica folder")
+  parser.add_argument("sync_interval", type=int, help="The synchronization interval in seconds")
+  parser.add_argument("log_file", help="The path to the log file")
+
+  args = parser.parse_args()
+
+  main(args.source_folder, args.replica_folder, args.sync_interval, args.log_file)
